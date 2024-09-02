ff5f60ba5b91b461130a54c4f4fc1ab556a04a9d
Ahmed-Boga <ahmedboga0@gmail.com>
2024-09-02 04:01:33 -0400

    initial commit

---
+++ Maths/mathematics.py
@@ -0,0 +1,15 @@
+# 
+def summation (a,b):
+    return a+b
+
+def subtraction(a,b):
+    return a-b
+
+def multiplication (a,b):
+    return a*b
+
+def division(a,b):
+    return a/b
+
+def powerof(a,b):
+    return a**b
